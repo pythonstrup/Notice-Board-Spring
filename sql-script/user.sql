beacd5673bf050c02df27453f6acbb48a11eba26
BigBell <pythonstrup@gmail.com>
2022-02-06 16:51:00 +0900

    user sql-script

---
+++ sql-script/user.sql
@@ -0,0 +1,36 @@
+DROP DATABASE  IF EXISTS `user`;
+
+CREATE DATABASE  IF NOT EXISTS `user`;
+USE `user`;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `users`
+--
+
+INSERT INTO `users` 
+VALUES 
+('john','{noop}test123',1),
+('mary','{noop}test123',1),
+('susan','{noop}test123',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+
+
+
